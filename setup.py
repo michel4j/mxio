77b831edf05fc3b4e8980cee01f8e1906206f945
Michel Fodje <michel4j@gmail.com>
2022-12-09 12:22:13 -0600

    Use setuptools_scm instead

--- setup.py
+++ setup.py
@@ -11,14 +11,9 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
-def package_version():
-    from mxio.version import get_version
-    return get_version()
-
-
 setup(
     name='mxio',
-    version=package_version(),
+    use_scm_version=True,
     packages=find_packages(),
     url='https://github.com/michel4j/mxio',
     include_package_data=True,
@@ -28,12 +23,13 @@ setup(
     description='A Simple MX Diffraction Image Library',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=requirements,
+    install_requires=requirements+ [
+        'importlib-metadata ~= 1.0 ; python_version < "3.8"', 'setuptools_scm'
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3',
     ],
 )
